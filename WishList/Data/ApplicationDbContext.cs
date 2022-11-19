f54580322109a9a3fa9acdbef419fbd48314e6ff
srividhiyaJ <118594248+srividhiyaJ@users.noreply.github.com>
2022-11-19 18:44:29 +0000

    Completed module

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        ApplicationDbContext(DbContextOptions options): base(options)
+        { }
+
+        public DbSet<Item> Item  {get; set; }
+
+}
+}
